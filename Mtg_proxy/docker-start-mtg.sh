944047ba3ccfb687e0b905fe738924599d811366
5points <1803962047@qq.com>
2020-10-22 11:05:03 +0800

    Update docker-start-mtg.sh

--- Mtg_proxy/docker-start-mtg.sh
+++ Mtg_proxy/docker-start-mtg.sh
@@ -1,3 +1,3 @@
 # if you prefer docker can run this command to generate a secret
 #docker run --rm nineseconds/mtg generate-secret tls -c bing.com
-docker run -d --restart always --name mtg --ulimit nofile=51200:51200 -p 8443:3128  nineseconds/mtg:latest run <secret>
+docker run -d --restart always --name mtg --ulimit nofile=51200:51200 -p <your port>:3128  nineseconds/mtg:latest run <secret>
