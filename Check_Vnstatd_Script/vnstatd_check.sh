810c8f196a2d639f423f5d2dc563a37a2f955484
5points <1803962047@qq.com>
2020-11-09 08:13:01 +0800

    Update vnstatd_check.sh

--- Check_Vnstatd_Script/vnstatd_check.sh
+++ Check_Vnstatd_Script/vnstatd_check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+CHECK_CMD="docker exec vnstatd vnstat"
+INPUT_CMD="docker exec -it vnstatd sh"
 until
         echo "Please enter your choise:(1-5)"
         echo "1.check vnstat date"
@@ -6,16 +8,18 @@ until
         echo "3.check vnstat hour"
         echo "4.check vnstat month"
         echo "5.check vnstat top/10day"
+#        echo "6.enter the container"
         echo "0.exit menu"
         read input
-        test $input = 6
+        test $input = 7
         do
                 case $input in
-                1)docker exec vnstatd vnstat -d;;
-                2)docker exec vnstatd vnstat -q;;
-                3)docker exec vnstatd vnstat -h;;
-                4)docker exec vnstatd vnstat -m;;
-                5)docker exec vnstatd vnstat -t;;
+                1) -d;;
+                2)CHECK_CMD -q;;
+                3)CHECK_CMD vnstat -h;;
+                4)CHECK_CMD vnstat -m;;
+                5)CHECK_CMD vnstat -t;;
+#                6)INPUT_CMD;;
                 0)exit;;
                 esac
                 done
