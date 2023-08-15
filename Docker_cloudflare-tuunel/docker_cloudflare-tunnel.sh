f14df535a818c366b6ad2c73812b1a0b4a81e51a
5points <8995110+5points@users.noreply.github.com>
2023-08-15 23:52:54 +0800

    Create docker_cloudflare-tunnel.sh

---
+++ Docker_cloudflare-tuunel/docker_cloudflare-tunnel.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+#!/usr/bin/env bash
+#step0-create cloudflard volume: docker volume create cloudflared
+#step1-create certificate: docker run -it --rm --privileged=true --user $(id -u) --net host -v cloudflared:/root/.cloudflared cloudflare/cloudflared tunnel login
+#step2-create tunnel name: docker run -it --rm --privileged=true --user $(id -u) --net host -v cloudflared:/root/.cloudflared cloudflare/cloudflared tunnel create <tunnelname>
+#step3-add tunnel route dns: docker run -it --rm --privileged=true --user $(id -u) --net host -v cloudflared:/root/.cloudflared cloudflare/cloudflared tunnel --config /root/.cloudflared/config.yml route dns <tunnelname> <tunnel-hostname>
+DOCKER_NAME=""
+CONFIG_NAME=""
+TUNNEL_NAME=""
+
+docker run -itd \
+   --name ${DOCKER_NAME} \
+   --privileged=true \
+   --user $(id -u) \
+   --net host \
+   -v cloudflared:/root/.cloudflared \
+   cloudflare/cloudflared \
+   tunnel --config /root/.cloudflared/${CONFIG_NAME} run ${TUNNEL_NAME}
