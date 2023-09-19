04f535b56e6ba3c163f2479f2284b37a2eb69e16
5points <8995110+5points@users.noreply.github.com>
2023-09-19 09:45:33 +0800

    Update docker_vnstatd.sh

--- Check_Vnstatd_Script/docker_vnstatd.sh
+++ Check_Vnstatd_Script/docker_vnstatd.sh
@@ -10,6 +10,7 @@ docker volume create vnstat_data
 echo vnstat_data create completed
 fi
 
+if [ $? -ne 0 ] && [[ "$(docker volume ls | grep -o vnstat_data)" = "$(echo vnstat_data)" ]];then
 docker run -d \
   --name vnstatd \
   --net=host \
@@ -18,3 +19,7 @@ docker run -d \
   -v /etc/localtime:/etc/localtime \
   -v vnstat_data:/var/lib/vnstat \
   carlosedp/vnstatd
+else
+echo have a something wrong
+exit 0
+fi
