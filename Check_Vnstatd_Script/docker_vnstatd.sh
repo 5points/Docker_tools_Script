0bcd4411e28cdb7ef3f88f59ec00930c4ffd8ac2
5points <8995110+5points@users.noreply.github.com>
2023-09-19 09:41:23 +0800

    Update docker_vnstatd.sh

--- Check_Vnstatd_Script/docker_vnstatd.sh
+++ Check_Vnstatd_Script/docker_vnstatd.sh
@@ -1,4 +1,15 @@
+#!/usr/bin/env sh
+#!/usr/bin/env bash
+#!/usr/bin/env zsh
+if [[ "$(docker volume ls | grep -o vnstat_data)" = "$(echo vnstat_data)" ]];then 
+echo have vnstat_data
+else
+echo not have vnstat_data...
+echo create vnstat_data
 docker volume create vnstat_data
+echo vnstat_data create completed
+fi
+
 docker run -d \
   --name vnstatd \
   --net=host \
