dc5dc6f6b99f26ab4b108beeb68c521691e1ac8f
5points <8995110+5points@users.noreply.github.com>
2024-03-27 22:24:09 +0800

    Update docker_dynu.sh

--- Docker_DDNS/docker_dynu.sh
+++ Docker_DDNS/docker_dynu.sh
@@ -7,16 +7,16 @@ read -p "Enter your domains that you want to update (separate multiple domains b
 read -p "Enter the filename for the new script (without extension): " -e FILENAME
 
 DOCKER_COMMAND=$(cat <<EOF
-docker run -itd \\
-   --name=dynuddns \\
-   --privileged=true \\
-   -e USERNAME=$USERNAME \\
-   -e PASSWORD=$PASSWORD \\
-   -e HOSTNAMES=$HOSTNAMES \\
-   -e REFRESH_TIME=30 \\
-   -e TZ='Asia/Hong_Kong' \\
-   --restart unless-stopped \\
-   --log-opt max-size=10k \\
+docker run -itd \
+   --name=dynuddns \
+   --privileged=true \
+   -e USERNAME=$USERNAME \
+   -e PASSWORD=$PASSWORD \
+   -e HOSTNAMES=$HOSTNAMES \
+   -e REFRESH_TIME=30 \
+   -e TZ='Asia/Hong_Kong' \
+   --restart unless-stopped \
+   --log-opt max-size=10k \
    dokeraj/dynu-updater:latest
 EOF
 )
@@ -24,7 +24,7 @@ EOF
 echo -e "Docker command to start the container:\n$DOCKER_COMMAND"
 
 # Save to Sh script
-echo -e "#!/bin/sh" > "$FILENAME.sh"
+echo -e "#!/usr/bin/env sh" > "$FILENAME.sh"
 echo -e "$DOCKER_COMMAND" >> "$FILENAME.sh"
 chmod +x "$FILENAME.sh"
 
