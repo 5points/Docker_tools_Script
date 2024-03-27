3c61cfcf1790366bfc3474e4e6a96f2a2be62c6c
5points <8995110+5points@users.noreply.github.com>
2024-03-27 22:07:00 +0800

    Update docker_dynu.sh

--- Docker_DDNS/docker_dynu.sh
+++ Docker_DDNS/docker_dynu.sh
@@ -1,14 +1,32 @@
+#!/usr/bin/env bash
 #-e USERNAME=This is your dynu login username.
 #-e PASSWORD=This is your dynu login password.
 #-e HOSTNAMES=This are your domains that you want to update.
-docker run -itd \
+
+read -p "Enter your Dynu login username: " USERNAME
+read -p "Enter your Dynu login password: " PASSWORD
+read -p "Enter your domains that you want to update (separate multiple domains by comma): " HOSTNAMES
+
+read -p "Enter the filename for the new script (without extension): " FILENAME
+
+DOCKER_COMMAND="docker run -itd \
    --name=dynuddns \
    --privileged=true \
-   -e USERNAME= \
-   -e PASSWORD=  \
-   -e HOSTNAMES=  \
+   -e USERNAME=$USERNAME \
+   -e PASSWORD=$PASSWORD \
+   -e HOSTNAMES=$HOSTNAMES \
    -e REFRESH_TIME=30 \
-   -e TZ="Asia/Hong_Kong" \
+   -e TZ='Asia/Hong_Kong' \
    --restart unless-stopped \
    --log-opt max-size=10k \
-   dokeraj/dynu-updater:latest
+   dokeraj/dynu-updater:latest"
+
+echo "Docker command to start the container:"
+echo "$DOCKER_COMMAND"
+
+# Save to Sh script
+echo "#!/bin/sh" > "$FILENAME.sh"
+echo "$DOCKER_COMMAND" >> "$FILENAME.sh"
+chmod +x "$FILENAME.sh"
+
+echo "New Docker start script has been saved to $FILENAME.sh"
