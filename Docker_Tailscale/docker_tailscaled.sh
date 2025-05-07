3846db7ef6037a0737498de74c1612e93bca298b
5points <8995110+5points@users.noreply.github.com>
2025-05-07 11:43:19 +0800

    Update docker_tailscaled.sh

--- Docker_Tailscale/docker_tailscaled.sh
+++ Docker_Tailscale/docker_tailscaled.sh
@@ -6,11 +6,21 @@
 #step2-enable exit node: tailscale up --accept-dns=false --advertise-exit-node or tailscale up --accept-dns=false
 #exit node guide: https://tailscale.com/kb/1103/exit-nodes?tab=linux
 
+CONTAINER_NAME="tailscaled"
+VOLUME_NAME="tailscale"
+
+echo "Check if the volume is enabled"
+if ! docker volume inspect "$VOLUME_NAME" >/dev/null 2>&1; then
+  echo "Creating volume $VOLUME_NAME..."
+  docker volume create "$VOLUME_NAME"
+  echo "$VOLUME_NAME volume create complete"
+fi
+
 docker run -itd \
-  --name=tailscaled \
+  --name="$CONTAINER_NAME" \
   --restart unless-stopped \
   --log-opt max-size=1m \
-  -v tailscale:/var/lib \
+  -v "VOLUME_NAME":/var/lib \
   -e TS_STATE_DIR=/var/lib/tailscale \
   --network=host \
   --cap-add=NET_ADMIN \
@@ -18,3 +28,5 @@ docker run -itd \
   --device=/dev/net/tun \
   tailscale/tailscale \
   tailscaled
+
+echo "Container $CONTAINER_NAME started successfully!"
