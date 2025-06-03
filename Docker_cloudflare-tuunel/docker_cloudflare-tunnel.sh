a91b257503afe4fd97e419c28659c4cbd935217a
5points <8995110+5points@users.noreply.github.com>
2025-06-03 14:49:59 +0800

    Update docker_cloudflare-tunnel.sh

--- Docker_cloudflare-tuunel/docker_cloudflare-tunnel.sh
+++ Docker_cloudflare-tuunel/docker_cloudflare-tunnel.sh
@@ -160,7 +160,7 @@ DOCKER_RUN_COMMAND=$(printf "%s\n" \
 "   --name ${DOCKER_NAME} \\" \
 "   --restart unless-stopped \\" \
 "   --log-opt max-size=10k \\" \
-"   --log-opt max-size=1 \\" \
+"   --log-opt max-file=1 \\" \
 "   --privileged=true \\" \
 "   --user \$(id -u) \\" \
 "   --net host \\" \
