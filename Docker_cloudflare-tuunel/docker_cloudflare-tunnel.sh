59dc865c303b4e549f5ce3e30c876c5e7a5d0063
5points <8995110+5points@users.noreply.github.com>
2025-06-03 14:38:19 +0800

    Update docker_cloudflare-tunnel.sh

--- Docker_cloudflare-tuunel/docker_cloudflare-tunnel.sh
+++ Docker_cloudflare-tuunel/docker_cloudflare-tunnel.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#step0-create cloudflard volume: docker volume create cloudflared
+#step1-create certificate: docker run -it --rm --privileged=true --user $(id -u) --net host -v cloudflared:/root/.cloudflared cloudflare/cloudflared tunnel login
+#step2-create tunnel name: docker run -it --rm --privileged=true --user $(id -u) --net host -v cloudflared:/root/.cloudflared cloudflare/cloudflared tunnel create <tunnelname>
+#step4-create tunnel config: wget -O $(docker volume inspect cloudflared | grep Mountpoint | awk '{print $NF}' | sed 's/[",]//g' | awk '{print $0 "/config.yml"}') https://github.com/5points/Docker_tools_Script/raw/main/Docker_cloudflare-tuunel/cloudflare-tunnel-config.yml
+#step4-edit tunnel config: vim $(docker volume inspect cloudflared | grep Mountpoint | awk '{print $NF}' | sed 's/[",]//g' | awk '{print $0 "/config.yml"}')
+#step5-add tunnel route dns: docker run -it --rm --privileged=true --user $(id -u) --net host -v cloudflared:/root/.cloudflared cloudflare/cloudflared tunnel --config /root/.cloudflared/config.yml route dns <tunnelname> <tunnel-hostname>
 
 ########################
 # Cloudflare Tunnel Installation and Configuration Script
